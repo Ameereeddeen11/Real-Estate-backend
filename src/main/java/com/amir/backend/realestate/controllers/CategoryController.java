6fb163ab2b556e46bd3a745b8a5864cd72563383
Amir <amir@Amirs-MacBook-Pro.local>
2026-09-16 17:52:27 +0200

    implement dto

--- src/main/java/com/amir/backend/realestate/controllers/CategoryController.java
+++ src/main/java/com/amir/backend/realestate/controllers/CategoryController.java
@@ -1,17 +1,21 @@
 package com.amir.backend.realestate.controllers;
 
+import com.amir.backend.realestate.domain.dtos.CategoryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
 public class CategoryController {
     @GetMapping
-    public void listCategories() {
+    public ResponseEntity<List<CategoryDto>> listCategories() {
         // ToDo: Implement the logic to list categories
+        return ResponseEntity.ok(List.of());
     }
 }
