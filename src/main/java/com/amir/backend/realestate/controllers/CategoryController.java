21028a2ce33ee544491f6a27de178ece283c27cf
Amir <amir@Amirs-MacBook-Pro.local>
2026-09-16 17:45:31 +0200

    creating endpoint for category

---
+++ src/main/java/com/amir/backend/realestate/controllers/CategoryController.java
@@ -0,0 +1,17 @@
+package com.amir.backend.realestate.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+    @GetMapping
+    public void listCategories() {
+        // ToDo: Implement the logic to list categories
+    }
+}
