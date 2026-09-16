806beb2091798f5ff2eedcf069680e71968ccd5c
Amir <amir@Amirs-MacBook-Pro.local>
2026-09-16 19:14:59 +0200

    create and implement mapper

--- src/main/java/com/amir/backend/realestate/controllers/CategoryController.java
+++ src/main/java/com/amir/backend/realestate/controllers/CategoryController.java
@@ -1,6 +1,9 @@
 package com.amir.backend.realestate.controllers;
 
 import com.amir.backend.realestate.domain.dtos.CategoryDto;
+import com.amir.backend.realestate.domain.entities.Category;
+import com.amir.backend.realestate.mappers.CategoryMapper;
+import com.amir.backend.realestate.services.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +16,13 @@ import java.util.List;
 @RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
 public class CategoryController {
+    private final CategoryMapper categoryMapper;
+    private final CategoryService categoryService;
+
     @GetMapping
     public ResponseEntity<List<CategoryDto>> listCategories() {
         // ToDo: Implement the logic to list categories
+        List<Category> categories = categoryService.listCategories();
         return ResponseEntity.ok(List.of());
     }
 }
